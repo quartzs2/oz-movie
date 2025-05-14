b35d5d064e221998c8df209833b0c0423b603d25
quartzs2 <csh001231@gmail.com>
2025-05-14 12:57:04 +0900

    design: 카드 컴포넌트 크기 변경(#5)
    - 카드 컴포넌트 크기 변경
    - 카드 컨테이너 반응형 조정
    - 메인 페이지 max-width 조정

--- src/components/MovieCardsContainer.jsx
+++ src/components/MovieCardsContainer.jsx
@@ -2,7 +2,7 @@ import MovieCard from "@components/MovieCard";
 
 const MovieCardsContainer = ({ movieListData }) => {
   return (
-    <section className="grid w-fit grid-cols-2 gap-2 pb-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+    <section className="grid w-fit grid-cols-1 gap-2 pb-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {movieListData?.map((movieData) => (
         <MovieCard key={movieData.id} {...movieData} />
       ))}
