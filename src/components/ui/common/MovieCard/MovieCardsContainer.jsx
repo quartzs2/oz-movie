d94c469b0bd8f11b7a5cf663851fce2e99c82da5
quartzs2 <csh001231@gmail.com>
2025-05-18 20:42:57 +0900

    feat: 무한 스크롤 구현(#27)
    - IntersectionObserver를 사용하기 위한 useIntersect 훅 구현
    - 영화 데이터를 상태로 관리하고, 데이터를 받아올 때마다 추가
    - 아이디가 중복되는 데이터를 제외하기 위해 set 사용
    - 영화 개수 변화에 맞춰서 스켈레톤을 표시하기 위해 MovieCardsContainer에 스켈레톤 포함
    - api/getPopularMovies 가 pageNumber를 파라미터로 받도록 수정

--- src/components/ui/common/MovieCard/MovieCardsContainer.jsx
+++ src/components/ui/common/MovieCard/MovieCardsContainer.jsx
@@ -1,11 +1,14 @@
 import MovieCard from "@components/ui/common/MovieCard/MovieCard";
+import MovieCardSkeleton from "@components/ui/common/MovieCard/MovieCardSkeleton";
 
-const MovieCardsContainer = ({ movieListData }) => {
+const MovieCardsContainer = ({ isLoading = false, movieListData, skeletonSize = 10 }) => {
   return (
     <section className="grid w-fit grid-cols-1 gap-2 pb-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {movieListData?.map((movieData) => (
         <MovieCard key={movieData.id} {...movieData} />
       ))}
+      {isLoading &&
+        Array.from({ length: skeletonSize }).map((_, idx) => <MovieCardSkeleton key={idx} />)}
     </section>
   );
 };
